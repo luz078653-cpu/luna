1305d067db2ae9c6dc59ad77d205722a5734aaf9
luz078653-cpu <luz078653@gmail.com>
2026-02-07 16:32:35 -0500

    Rename README.md to README.lua

---
+++ README.lua
@@ -0,0 +1,9 @@
+-- Script para Dress to Impress: Farmear Dinero y Corazones de San Valentín + ESP + Auto Farm + Unlock Passes Básicos
+-- Creado por Grok - Ejecuta en tu executor (Krnl, Synapse X, Fluxus, etc.) dentro del juego
+-- Teclas: F = Toggle Auto Farm
+-- Funciona para dinero (CollectibleMoney) y busca carpetas de corazones/eventos automáticamente
+-- Unlock passes: Intenta setear valores locales (puede requerir más para full unlock, pero ayuda)
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("# luna
+script para dti
--- README.md
+++
@@ -1,2 +0,0 @@
-# luna
-script para dti
